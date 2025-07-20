d6a047c3b44a9f7da65ad0349878d271ebcd7c57
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-07-20 18:45:22 +0500

    first commit

---
+++ Visitor/index.php
@@ -0,0 +1,12 @@
+<?php
+require_once "../VisitorLayout/header.php";
+?>
+
+<!-- Banner -->
+<div class="w-full h-150 bg-cover flex flex-col justify-center items-center text-white" style="background-image:url('../Assets/home_banner.png')">
+    <h1 class="text-6xl font-medium">Drive <span class="text-[#EDC549]">Dubai</span> in <span class="text-[#EDC549]">Style</span></h1>
+    <p class="text-2xl font-medium">Rent luxury, exotic, and sports cars from the #1 premium car rental service in Dubai.</p>
+</div>
+<?php
+require_once "../VisitorLayout/footer.php";
+?>
---
+++ VisitorLayout/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+        rel="stylesheet">
+    <title>Home</title>
+    <style>
+        body{
+            font-family: "Inter", sans-serif;
+            background-color: #BFBFBF;
+        }
+    </style>
+</head>
+
+<!-- Header -->
+<div class="w-full bg-[#121212] text-white py-4 flex flex-row justify-end">
+    <div>
+        <img src="" alt="">
+    </div>
+    <div class="space-x-8 mr-5 font-light">
+        <a href="" class="hover:text-[#EDC549]">Home</a>
+        <a href="" class="hover:text-[#EDC549]">Car Listing</a>
+        <a href="" class="hover:text-[#EDC549]">About Us</a>
+        <a href="" class="hover:text-[#EDC549]">Contact Us</a>
+        <a href="" class="bg-[#7B5D01] text-white rounded-3xl px-5 py-1">Sign Up</a>
+    </div>
+</div>
+
+<body>

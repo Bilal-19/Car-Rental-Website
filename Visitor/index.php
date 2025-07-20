928d0245b902b749e71b84f12d5333495065d52c
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-07-20 19:24:26 +0500

    Updated header and home page.

--- Visitor/index.php
+++ Visitor/index.php
@@ -3,9 +3,20 @@ require_once "../VisitorLayout/header.php";
 ?>
 
 <!-- Banner -->
-<div class="w-full h-150 bg-cover flex flex-col justify-center items-center text-white" style="background-image:url('../Assets/home_banner.png')">
-    <h1 class="text-6xl font-medium">Drive <span class="text-[#EDC549]">Dubai</span> in <span class="text-[#EDC549]">Style</span></h1>
-    <p class="text-2xl font-medium">Rent luxury, exotic, and sports cars from the #1 premium car rental service in Dubai.</p>
+<div class="w-full h-80 md:h-180 bg-cover flex flex-col justify-center items-center text-white"
+    style="background-image:url('../Assets/home_banner.png')">
+    <div class="md:w-4/5 mx-auto text-center">
+        <h1 class="text-4xl md:text-6xl font-medium mb-2">Drive <span class="text-[#EDC549]">Dubai</span> in <span
+                class="text-[#EDC549]">Style</span></h1>
+        <p class="md:w-4/6 mx-auto text-base md:text-2xl font-medium">Rent luxury, exotic, and sports cars from the #1
+            premium car rental service in
+            Dubai.</p>
+    </div>
+</div>
+
+<div class="container-fluid my-20 text-center">
+    <h3 class="text-5xl font-light">Why Choose <span class="text-[#7B5D01] font-medium">Us?</span></h3>
+    <p class="text-2xl font-light">Why Rent With Us?</p>
 </div>
 <?php
 require_once "../VisitorLayout/footer.php";
--- VisitorLayout/header.php
+++ VisitorLayout/header.php
@@ -10,9 +10,12 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
         rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Home</title>
     <style>
-        body{
+        body {
             font-family: "Inter", sans-serif;
             background-color: #BFBFBF;
         }
@@ -20,11 +23,19 @@
 </head>
 
 <!-- Header -->
-<div class="w-full bg-[#121212] text-white py-4 flex flex-row justify-end">
-    <div>
-        <img src="" alt="">
+<div
+    class="w-full bg-[#121212] text-white py-2 flex flex-col md:flex-row justify-between md:items-center border-b-2 border-b-[#EDC549]">
+    <div class="flex flex-row justify-between">
+        <div>
+            <img src="../Assets/logo.png" alt="logo" class="h-12">
+        </div>
+        <div>
+            <a href="" class="block md:hidden">
+                <i class="fa-solid fa-bars"></i>
+            </a>
+        </div>
     </div>
-    <div class="space-x-8 mr-5 font-light">
+    <div class="md:space-x-8 md:mr-5 text-sm md:text-base font-light flex flex-col md:flex-row space-y-1 md:space-y-0 ml-3 md:ml-0">
         <a href="" class="hover:text-[#EDC549]">Home</a>
         <a href="" class="hover:text-[#EDC549]">Car Listing</a>
         <a href="" class="hover:text-[#EDC549]">About Us</a>

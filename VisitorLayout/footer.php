9c7c7aa32e894bf9dac3816ff303c455a0f02159
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-07-23 19:35:03 +0500

    Updated header.

--- VisitorLayout/footer.php
+++ VisitorLayout/footer.php
@@ -1,2 +1,11 @@
+<script>
+    const toggleBtn = document.getElementById("toggleBtn")
+    const menus = document.getElementById("toggleMenus");
+    toggleBtn.addEventListener("click", () => {
+        menus.classList.toggle("hidden")
+    })
+</script>
+
 </body>
+
 </html>
--- VisitorLayout/header.php
+++ VisitorLayout/header.php
@@ -22,26 +22,27 @@
     </style>
 </head>
 
-<!-- Header -->
-<div
-    class="w-full bg-[#121212] text-white py-2 flex flex-col md:flex-row justify-between md:items-center border-b-2 border-b-[#EDC549]">
-    <div class="flex flex-row justify-between">
-        <div>
-            <img src="../Assets/logo.png" alt="logo" class="h-12">
-        </div>
-        <div>
-            <a href="" class="block md:hidden">
-                <i class="fa-solid fa-bars"></i>
-            </a>
-        </div>
-    </div>
-    <div class="md:space-x-8 md:mr-5 text-sm md:text-base font-light flex flex-col md:flex-row space-y-1 md:space-y-0 ml-3 md:ml-0">
-        <a href="" class="hover:text-[#EDC549]">Home</a>
-        <a href="" class="hover:text-[#EDC549]">Car Listing</a>
-        <a href="" class="hover:text-[#EDC549]">About Us</a>
-        <a href="" class="hover:text-[#EDC549]">Contact Us</a>
-        <a href="" class="bg-[#7B5D01] text-white rounded-3xl px-5 py-1">Sign Up</a>
-    </div>
-</div>
-
 <body>
+
+    <!-- Header -->
+    <div
+        class="w-full bg-[#121212] text-white py-2 flex flex-col md:flex-row justify-between md:items-center border-b-2 border-b-[#EDC549]">
+        <div class="flex flex-row justify-between items-center">
+            <div>
+                <img src="../Assets/logo.png" alt="logo" class="h-12">
+            </div>
+            <div>
+                <button class="block md:hidden border border-2 rounded-md px-3 py-1" id="toggleBtn">
+                    <i class="fa-solid fa-bars"></i>
+                </button>
+            </div>
+        </div>
+        <div class="hidden flex-col md:flex md:flex-row space-y-1 md:space-y-0 md:space-x-8 md:mr-5 text-sm md:text-base font-light px-4"
+            id="toggleMenus">
+            <a href="" class="hover:text-[#EDC549] block md:inline">Home</a>
+            <a href="" class="hover:text-[#EDC549] block md:inline">Car Listing</a>
+            <a href="" class="hover:text-[#EDC549] block md:inline">About Us</a>
+            <a href="" class="hover:text-[#EDC549] block md:inline">Contact Us</a>
+            <a href="" class="bg-[#7B5D01] text-white rounded-3xl px-5 py-1 block">Sign Up</a>
+        </div>
+    </div>

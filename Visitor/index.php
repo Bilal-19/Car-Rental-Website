3db64d94806bd79757bc66bbb57e342093131165
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-07-28 19:30:36 +0500

    Created responsive footer.

--- Visitor/index.php
+++ Visitor/index.php
@@ -133,7 +133,8 @@ require_once "../VisitorLayout/header.php";
     </div>
 </div>
 
-<div class="mt-30 w-full h-52 md:h-100 text-white flex flex-col justify-center text-start p-10" style="background-image:url('../Assets/footer_cta_banner.png')">
+<div class="mt-30 w-full h-52 md:h-100 text-white flex flex-col justify-center text-start p-10"
+    style="background-image:url('../Assets/footer_cta_banner.png')">
     <h3 class="text-xl md:text-4xl mb-3">Ready to Elevate Your Dubai Experience?</h3>
     <button class="bg-[#7B5D01] w-fit px-3 py-3 rounded-xl">Book Your Luxury Car Now</button>
 </div>
--- VisitorLayout/footer.php
+++ VisitorLayout/footer.php
@@ -6,6 +6,28 @@
     })
 </script>
 
+<div class="w-full bg-[#1E1E1E] text-white flex flex-col md:flex-row justify-between items-start md:text-xl p-5 md:p-15 space-y-10 md:space-y-0">
+    <div>
+        <img src="../Assets/logo.png" alt="logo" class="h-12">
+    </div>
+    <div>
+        <h5 class="font-medium">Quick Links</h5>
+        <div class="font-extralight flex flex-col">
+            <a href="">Home</a>
+            <a href="">Car Listing</a>
+            <a href="">About Us</a>
+            <a href="">Contact Us</a>
+        </div>
+    </div>
+    <div>
+        <h5>Contact Us</h5>
+        <div class="font-extralight flex flex-col">
+            <a href="">+971-50-1234567</a>
+            <a href="">support@eliteautomotors.com</a>
+            <a href="">Dubai Marina, UAE</a>
+        </div>
+    </div>
+</div>
 </body>
 
 </html>

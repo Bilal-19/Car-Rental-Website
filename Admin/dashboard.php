effeb3f4c0871052e70e17c2b86b17b33b63fc18
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-12-28 19:25:55 +0500

    Updated admin dashboard.

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -1,32 +1,48 @@
-<?php 
+<?php
 include("../AdminLayout/header.php");
 include("../AdminLayout/sidebar.php");
- ?>
+?>
 <main class="flex-1 p-6 overflow-x-auto">
     <div class="w-full mt-5 bg-white rounded p-6">
         <h2 class="text-xl font-semibold mb-5">Admin Dashboard</h2>
 
         <div class="grid grid-cols-1 md:grid-cols-3 gap-10">
             <div class="bg-gray-200 rounded-md p-5 text-center border-x-1 border-b-4 border-gray-900">
-                <h3 class="font-medium text-xl">Total Visa Applicants</h3>
-                <p class="md:text-lg">
-                    <i class="fa-solid fa-users-between-lines text-blue-700"></i> {{ $countApplicants }}
+                <h3 class="font-medium text-lg">Total Vehicles</h3>
+                <p class="md:text-md">
+                    <i class="fa-solid fa-car text-blue-700"></i> 5000
                 </p>
             </div>
 
             <div class="bg-gray-200 rounded-md p-5 text-center border-x-1 border-b-4 border-gray-900">
-                <h3 class="font-medium text-xl">Approved Applicants</h3>
-                <p class="md:text-lg">
-                    <i class="fa-solid fa-users-line text-green-700"></i> {{ $approvedApplicants }}
+                <h3 class="font-medium text-lg">Booked Vehicles</h3>
+                <p class="md:text-md">
+                    <i class="fas fa-car-side text-red-700"></i> 3500
                 </p>
             </div>
 
             <div class="bg-gray-200 rounded-md p-5 text-center border-x-1 border-b-4 border-gray-900">
-                <h3 class="font-medium text-xl">Reject Applicants</h3>
-                <p class="md:text-lg">
-                    <i class="fa-solid fa-users-slash text-red-700"></i> {{ $rejectedApplicants }}
+                <h3 class="font-medium text-lg">Available Vehicles</h3>
+                <p class="md:text-md">
+                    <i class="fas fa-car-side text-green-700"></i> 1500
                 </p>
             </div>
+
+            <div class="bg-gray-200 rounded-md p-5 text-center border-x-1 border-b-4 border-gray-900">
+                <h3 class="font-medium text-lg">Total Users</h3>
+                <p class="md:text-md">
+                    <i class="fa-solid fa-users text-green-700"></i> 300
+                </p>
+            </div>
+
+            <div class="bg-gray-200 rounded-md p-5 text-center border-x-1 border-b-4 border-gray-900">
+                <h3 class="font-medium text-lg">Total Enquiries</h3>
+                <p class="md:text-md">
+                    <i class="fa-solid fa-question text-red-700"></i> 50
+                </p>
+            </div>
+
+
         </div>
 
         <div class="w-full flex flex-col md:flex-row md:justify-between my-3">
--- AdminLayout/sidebar.php
+++ AdminLayout/sidebar.php
@@ -3,7 +3,7 @@
         <!-- Sidebar -->
         <aside id="sidebar"
             class="bg-[#7B5D01] text-black w-64 space-y-4 py-6 px-4 absolute md:relative z-10 top-16 left-0 md:top-0 md:flex md:flex-col transform -translate-x-full md:translate-x-0 transition-transform duration-200 ease-in-out h-min-screen">
-            <nav class="space-y-10 text-sm">
+            <nav class="space-y-14 text-sm">
                 <a href="../Admin/dashboard.php"
                     class="flex items-center space-x-2 px-4 py-2 border-b bg-white text-black rounded hover:bg-gray-200">
                     <i class="fa-solid fa-house"></i>

29dffb675d8cc1ccb39915cfe6a7bd34fa1b0b80
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2026-04-01 21:28:18 +0500

    Make vehicle listing table responsive for small screen.

--- Admin/vehicles.php
+++ Admin/vehicles.php
@@ -4,7 +4,7 @@ include("../AdminLayout/sidebar.php");
 
 require_once "../DB/db_connection.php";
 
-$listVehiclesQry = "SELECT * FROM vehicles";
+$listVehiclesQry = "SELECT * FROM vehicles LIMIT 10";
 $listVehiclesRes = mysqli_query($isConnect, $listVehiclesQry);
 
 ?>
@@ -12,44 +12,52 @@ $listVehiclesRes = mysqli_query($isConnect, $listVehiclesQry);
     <div class="w-full mt-5 bg-white rounded p-6">
         <h2 class="text-xl font-semibold mb-5">All Vehicles</h2>
 
-        <div class="flex justify-between">
+        <div class="flex flex-col md:flex-row justify-between">
             <p><?php echo $listVehiclesRes->num_rows; ?> records found</p>
-            <button class="bg-blue-500 text-white text-sm px-3 py-1 rounded-md">
+            <button class="bg-blue-500 text-white text-xs md:text-sm px-3 py-1 rounded-md inline-block">
                 <a href="./add_vehicle.php">Add New Vehicle</a>
             </button>
         </div>
 
-        <table class="w-full mt-5">
-            <thead class="text-sm text-left">
-                <tr class="border-b border-gray-900">
-                    <th>ID</th>
-                    <th>Make / Model</th>
-                    <th>Category / Transmission</th>
-                    <th>Engine Capacity</th>
-                    <th>Per Day Cost</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-
-            <tbody class="text-xs">
-                <?php
-                while ($row = mysqli_fetch_assoc($listVehiclesRes)) { ?>
-                    <!-- echo "<pre>"; -->
-                    <!-- print_r($row); -->
-                    <tr class="border-b border-gray-500 py-4 h-4 hover:bg-gray-300">
-                        <td><?php echo $row['id'] ?></td>
-                        <td><?php echo $row['make'] . '&nbsp|&nbsp' . $row['model']; ?></td>
-                        <td><?php echo $row['category'] . '<br>' . $row['transmission']; ?></td>
-                        <td><?php echo $row['engine_capacity'] . " CC"; ?></td>
-                        <td><?php echo floor($row['per_day_cost']) . " AED / day"; ?></td>
-                        <td>
-                            <a href="<?php echo $row['id']; ?>">Edit</a>
-                        </td>
+        <!-- Table responsive won't work if you remove 'whitespace-nowrap' class -->
+        <div class="overflow-x-auto relative">
+            <table class="w-full">
+                <thead class="text-xs md:text-sm text-left whitespace-nowrap">
+                    <tr class="border-b-2 border-gray-900">
+                        <th class="p-3">ID</th>
+                        <th class="p-3">Make</th>
+                        <th class="p-3">Model</th>
+                        <th class="p-3">Category</th>
+                        <th class="p-3">Transmission</th>
+                        <th class="p-3">Engine Capacity</th>
+                        <th class="p-3">Per Day Cost</th>
+                        <th class="p-3">Actions</th>
                     </tr>
-                <?php } ?>
-            </tbody>
-        </table>
+                </thead>
 
+                <tbody class="text-xs whitespace-nowrap">
+                    <?php
+                    while ($row = mysqli_fetch_assoc($listVehiclesRes)) { ?>
+                        <!-- echo "<pre>"; -->
+                        <!-- print_r($row); -->
+                        <tr class="border-b border-gray-600 hover:bg-gray-300">
+                            <td class="p-2 border-x"><?php echo $row['id'] ?></td>
+                            <td class="p-2 border-x"><?php echo $row['make']; ?></td>
+                            <td class="p-2 border-x"><?php echo $row['model']; ?></td>
+                            <td class="p-2 border-x"><?php echo $row['category']; ?></td>
+                            <td class="p-2 border-x"><?php echo $row['transmission']; ?></td>
+                            <td class="p-2 border-x"><?php echo $row['engine_capacity'] . " CC"; ?></td>
+                            <td class="p-2 border-x"><?php echo floor($row['per_day_cost']) . " AED / day"; ?></td>
+                            <td class="p-2 border-x text-center">
+                                <a href="<?php echo $row['id']; ?>"><i class="fa-regular fa-pen-to-square text-blue-600"></i></a>
+                                &nbsp;&nbsp;
+                                <a href="<?php echo $row['id']; ?>"><i class="fa-solid fa-trash-arrow-up text-red-600"></i></a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
 
     </div>
 </main>

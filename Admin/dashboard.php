392ff1882c30a9f72ec7a5b3098c0de5cd9ddac8
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2026-04-05 08:48:56 +0500

    Display dynamic vehicle statistics (total, booked, available).

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -1,6 +1,19 @@
 <?php
 include("../AdminLayout/header.php");
 include("../AdminLayout/sidebar.php");
+
+require_once "../DB/db_connection.php";
+
+// Find count of no of vehicles
+$noOfVehicleRes = mysqli_query($isConnect, "SELECT count(*) as total FROM vehicles");
+$noOfVehicleCount = mysqli_fetch_assoc($noOfVehicleRes);
+$countVehicles = $noOfVehicleCount['total'];
+
+// Find no of booked vehicles
+$noOfBookedVehicleRes = mysqli_query($isConnect, "SELECT count(DISTINCT vehicle_id) as total FROM vehicle_booking");
+$noOfBookedVehicleResCount = mysqli_fetch_assoc($noOfBookedVehicleRes);
+$countBookedVehicles = $noOfBookedVehicleResCount['total'];
+
 ?>
 <main class="flex-1 p-6 overflow-x-auto">
     <div class="w-full mt-5 bg-white rounded p-6">
@@ -10,21 +23,21 @@ include("../AdminLayout/sidebar.php");
             <div class="bg-gray-200 rounded-md p-5 text-center border-x-1 border-b-4 border-gray-900">
                 <h3 class="font-medium text-lg">Total Vehicles</h3>
                 <p class="md:text-md">
-                    <i class="fa-solid fa-car text-blue-700"></i> 5000
+                    <i class="fa-solid fa-car text-blue-700"></i> <?php echo $countVehicles; ?>
                 </p>
             </div>
 
             <div class="bg-gray-200 rounded-md p-5 text-center border-x-1 border-b-4 border-gray-900">
                 <h3 class="font-medium text-lg">Booked Vehicles</h3>
                 <p class="md:text-md">
-                    <i class="fas fa-car-side text-red-700"></i> 3500
+                    <i class="fas fa-car-side text-red-700"></i> <?php echo $countBookedVehicles; ?>
                 </p>
             </div>
 
             <div class="bg-gray-200 rounded-md p-5 text-center border-x-1 border-b-4 border-gray-900">
                 <h3 class="font-medium text-lg">Available Vehicles</h3>
                 <p class="md:text-md">
-                    <i class="fas fa-car-side text-green-700"></i> 1500
+                    <i class="fas fa-car-side text-green-700"></i> <?php echo ($countVehicles - $countBookedVehicles) ?>
                 </p>
             </div>
 

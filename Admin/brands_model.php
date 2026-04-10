3a31a3975680c00f46c0bdae893f9358e035ddb1
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2026-04-10 20:20:51 +0500

    Listed vehicle brands.

--- Admin/brands_model.php
+++ Admin/brands_model.php
@@ -6,6 +6,9 @@ require_once "../DB/db_connection.php";
 require_once "../utilities.php";
 
 
+$vehicleBrandsQry = "SELECT * FROM vehicle_brands LIMIT 10";
+$vehicleBrandsRes = mysqli_query($isConnect, $vehicleBrandsQry);
+
 ?>
 <main class="flex-1 p-6 overflow-x-auto">
     <div class="w-full mt-5 bg-white rounded p-6">
@@ -27,16 +30,33 @@ require_once "../utilities.php";
             <table class="w-full">
                 <thead class="text-xs md:text-sm text-left whitespace-nowrap bg-gray-300">
                     <tr class="border-b-2 border-gray-900 text-center">
-                        <th class="p-3">#</th>
-                        <th class="p-3">Brand Name</th>
-                        <th class="p-3">No of Associated Models</th>
-                        <th class="p-3">Edit</th>
-                        <th class="p-3">Delete</th>
+                        <th class="p-2">#</th>
+                        <th class="p-2" align="left">Brand Name</th>
+                        <th class="p-2">No of Associated Models</th>
+                        <th class="p-2">Edit</th>
+                        <th class="p-2">Delete</th>
                     </tr>
                 </thead>
 
-                <tbody class="text-xs whitespace-nowrap">
+                <tbody class="text-xs whitespace-nowrap text-center">
+                    <?php
+                    if (mysqli_num_rows($vehicleBrandsRes) > 0) {
+                        while ($row = mysqli_fetch_assoc($vehicleBrandsRes)) { ?>
+                            <tr class="border-b border-gray-600 hover:bg-gray-200">
+                                <td class="p-2"><?php echo $row['id']; ?></td>
+                                <td class="p-2" align="left"><?php echo $row['brand_name']; ?></td>
+                                <td class="p-2"><?php echo 5; ?></td>
+                                <td class="p-2">
+                                    <button class="edit_brand" value="<?php echo $row['id']; ?>"><i class="fa-regular fa-pen-to-square text-blue-700"></i></button
+                                </td>
+                                <td class="p-2">
+                                    <button class="del_brand" value="<?php echo $row['id']; ?>"><i class="fa-solid fa-trash-arrow-up text-red-700"></i></button>
+                                </td>
+                            </tr>
 
+                        <?php }
+                    }
+                    ?>
                 </tbody>
             </table>
         </div>
@@ -72,8 +92,8 @@ require_once "../utilities.php";
                     }
                 }
             })
-
-
         })
+
+        
     })
 </script>

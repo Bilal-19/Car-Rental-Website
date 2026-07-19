fe6560063bd8c77e9b2708aeffb37af6e0b918f1
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2026-07-19 19:16:50 +0500

    implement user-friendly 404 view for missing vehicles

--- Visitor/car_details.php
+++ Visitor/car_details.php
@@ -11,10 +11,13 @@ $vehicleID = $_GET['id'];
 $vehicleQry = "SELECT * FROM vehicles WHERE id = " . $vehicleID;
 $vehicleRes = mysqli_query($isConnect, $vehicleQry);
 $vehicleResArr = mysqli_fetch_assoc($vehicleRes);
-// print_r($vehicleResArr);
 
+
+if (mysqli_num_rows($vehicleRes) > 0){
 // Fetch vehicle multiple images
-$vehicleImgsRes = mysqli_query($isConnect, "SELECT * FROM vehicle_images WHERE vehicle_id = " . $vehicleResArr['id']);
+    $vehicleImgsRes = mysqli_query($isConnect, "SELECT * FROM vehicle_images WHERE vehicle_id = " . $vehicleResArr['id']);
+}
+
 // $vehicleImgsResArr = mysqli_fetch_array($vehicleImgsRes);
 // print_r($vehicleImgsResArr);
 
@@ -27,7 +30,7 @@ $availableLoc = array('Business Bay', 'Sheikh Zayed Road');
 <!-- Take image slideshow reference -->
 <!-- https://www.w3schools.com/howto/howto_js_slideshow.asp -->
 
-<?php if (mysqli_num_rows($vehicleImgsRes) > 1) { ?>
+<?php if (mysqli_num_rows($vehicleRes) > 0 && mysqli_num_rows($vehicleImgsRes) > 1) { ?>
     <div class="w-full relative mx-auto my-10">
         <?php
         while ($row = mysqli_fetch_array($vehicleImgsRes)) { ?>
@@ -46,183 +49,211 @@ $availableLoc = array('Business Bay', 'Sheikh Zayed Road');
     </div>
 <?php } ?>
 
-<div class="w-full my-20">
-    <h3 class="md:w-4/5 mx-auto my-5 text-xl md:text-4xl text-[#7B5D01] font-medium">
-        <?php echo $vehicleResArr['make'] . " . " . $vehicleResArr['model']; ?>
-    </h3>
-    <h5 class="md:w-4/5 mx-auto my-5 text-md md:text-2xl">Vehicle <span
-            class="text-[#7B5D01] font-medium">Overview</span></h5>
-    <table class="md:w-4/5 text-left mx-auto">
-        <tr class="border-b">
-            <th class="py-2 font-medium">Make & Model</th>
-            <td class="font-light"><?php echo $vehicleResArr['make'] . " . " . $vehicleResArr['model']; ?></td>
-        </tr>
-
-        <tr class="border-b">
-            <th class="py-2 font-medium">Engine Capacity</th>
-            <td class="font-light"><?php echo $vehicleResArr['engine_capacity'] . " CC"; ?></td>
-        </tr>
-
-        <tr class="border-b">
-            <th class="py-2 font-medium">Category</th>
-            <td class="font-light"><?php echo $vehicleResArr['category']; ?></td>
-        </tr>
-
-        <tr class="border-b">
-            <th class="py-2 font-medium">Transmision</th>
-            <td class="font-light"><?php echo $vehicleResArr['transmission']; ?></td>
-        </tr>
-
-        <tr class="border-b">
-            <th class="py-2 font-medium">Trim</th>
-            <td class="font-light"><?php echo $vehicleResArr['TRIM']; ?></td>
-        </tr>
-
-        <tr class="border-b">
-            <th class="py-2 font-medium">Horsepower</th>
-            <td class="font-light"><?php echo $vehicleResArr['horsepower'] . " hp"; ?></td>
-        </tr>
-
-        <tr class="border-b">
-            <th class="py-2 font-medium">Doors</th>
-            <td class="font-light"><?php echo $vehicleResArr['doors']; ?></td>
-        </tr>
-
-        <tr class="border-b">
-            <th class="py-2 font-medium">Fuel Type</th>
-            <td class="font-light"><?php echo $vehicleResArr['fuel_type']; ?></td>
-        </tr>
-
-        <tr class="border-b">
-            <th class="py-2 font-medium">No of Cylinders</th>
-            <td class="font-light"><?php echo $vehicleResArr['no_of_cylinders']; ?></td>
-        </tr>
-
-        <tr class="border-b">
-            <th class="py-2 font-medium">Interior Color</th>
-            <td class="font-light"><?php echo $vehicleResArr['interior_color']; ?></td>
-        </tr>
-
-        <tr class="border-b">
-            <th class="py-2 font-medium">Exterior Color</th>
-            <td class="font-light"><?php echo $vehicleResArr['exterior_color']; ?></td>
-        </tr>
-
-        <tr class="border-b">
-            <th class="py-2 font-medium">Per Day Cost</th>
-            <td class="font-light"><?php echo floor($vehicleResArr['per_day_cost']) . " AED / day"; ?></td>
-        </tr>
-
-        <tr class="border-b">
-            <th class="py-2 font-medium">Drive Type</th>
-            <td class="font-light"><?php echo $vehicleResArr['drive_type']; ?></td>
-        </tr>
-
-        <tr class="border-b">
-            <th class="py-2 font-medium">Seating Capacity</th>
-            <td class="font-light"><?php echo $vehicleResArr['seating_capacity']; ?></td>
-        </tr>
-
-        <tr class="border-b">
-            <th class="py-2 font-medium">Registration Number</th>
-            <td class="font-light"><?php echo $vehicleResArr['registration_number']; ?></td>
-        </tr>
-    </table>
-</div>
-
-<!-- Show vehicle booking form only when vehicle is available -->
 <?php
-$isBookRes = mysqli_query($isConnect, "SELECT * FROM vehicle_booking WHERE return_date < NOW() AND vehicle_id = " . $vehicleResArr['id']);
-$isBookResArr = mysqli_fetch_assoc($isBookRes);
-$bookedCount = mysqli_num_rows($isBookRes);
 
-if ($bookedCount == 0) { ?>
-    <div class="w-full h-200 md:h-fit bg-cover flex flex-col justify-center items-center text-white mt-30 py-10 bg-no-repeat bg-scroll"
-        style="background-image:url('../Assets/contact_form_bg.png')">
-        <h4 class="text-2xl md:text-[45px] font-semibold mb-2 text-center my-20">Book This Car Now</h4>
-        <div class="w-80 md:w-4/5 mx-auto md:mb-5">
-            <form name="bookVehicle" id="bookVehicle" method="post" class="space-y-5">
-                <input type="hidden" name="vehicle_id" id="vehicle_id" value="<?php echo $vehicleID; ?>">
-                <div>
-                    <input type="text" id="pickup_date"
-                        class="bg-white text-black px-3 md:px-6 py-3 rounded-2xl focus:outline-none block w-80 md:w-4/5 mx-auto"
-                        name="pickup_date" placeholder="Select Pickup Date" onfocus="(this.type)='date'">
-                    <span class="text-sm font-medium w-80 md:w-4/5 mx-auto block">
-                        <?php if (!empty($error["nameErr"])) {
-                            echo $error["nameErr"];
-                        } ?>
-                    </span>
-                </div>
+if (mysqli_num_rows($vehicleRes) > 0) { ?>
+    <div class="w-full my-20">
+        <h3 class="md:w-4/5 mx-auto my-5 text-xl md:text-4xl text-[#7B5D01] font-medium">
+            <?php echo $vehicleResArr['make'] . " . " . $vehicleResArr['model']; ?>
+        </h3>
+        <h5 class="md:w-4/5 mx-auto my-5 text-md md:text-2xl">Vehicle <span
+                class="text-[#7B5D01] font-medium">Overview</span></h5>
+        <table class="md:w-4/5 text-left mx-auto">
+            <tr class="border-b">
+                <th class="py-2 font-medium">Make & Model</th>
+                <td class="font-light"><?php echo $vehicleResArr['make'] . " . " . $vehicleResArr['model']; ?></td>
+            </tr>
 
-                <div>
-                    <input type="text" id="return_date"
-                        class="bg-white text-black px-3 md:px-6 py-3 rounded-2xl focus:outline-none block w-80 md:w-4/5 mx-auto"
-                        name="return_date" placeholder="Select Return Date" onfocus="(this.type)='date'">
-                    <span class="text-sm font-medium w-80 md:w-4/5 mx-auto block">
-                        <?php if (!empty($error["emailErr"])) {
-                            echo $error["emailErr"];
-                        } ?>
-                    </span>
-                </div>
+            <tr class="border-b">
+                <th class="py-2 font-medium">Engine Capacity</th>
+                <td class="font-light"><?php echo $vehicleResArr['engine_capacity'] . " CC"; ?></td>
+            </tr>
 
-                <div>
-                    <select
-                        class="bg-white text-black px-3 md:px-6 py-3 rounded-2xl focus:outline-none block w-80 md:w-4/5 mx-auto appearance-none"
-                        name="pickup_location" id="pickup_location">
-                        <option value="">Select Pickup Location</option>
-                        <?php
-                        foreach ($availableLoc as $val) { ?>
-                            <option value="<?php echo $val; ?>"><?php echo $val; ?></option>
-                        <?php }
-                        ?>
-                    </select>
-                    <span class="text-sm font-medium w-80 md:w-4/5 mx-auto block">
-                        <?php if (!empty($error["phoneErr"])) {
-                            echo $error["phoneErr"];
-                        } ?>
-                    </span>
-                </div>
+            <tr class="border-b">
+                <th class="py-2 font-medium">Category</th>
+                <td class="font-light"><?php echo $vehicleResArr['category']; ?></td>
+            </tr>
 
-                <div>
-                    <select
-                        class="bg-white text-black px-3 md:px-6 py-3 rounded-2xl focus:outline-none block w-80 md:w-4/5 mx-auto appearance-none"
-                        name="is_driver_needed" id="is_driver_needed">
-                        <option value="">Need a Driver?</option>
-                        <option value="Yes">Yes</option>
-                        <option value="No">No</option>
-                    </select>
-                    <span class="text-sm font-medium w-80 md:w-4/5 mx-auto block">
-                        <?php if (!empty($error["phoneErr"])) {
-                            echo $error["phoneErr"];
-                        } ?>
-                    </span>
-                </div>
+            <tr class="border-b">
+                <th class="py-2 font-medium">Transmision</th>
+                <td class="font-light"><?php echo $vehicleResArr['transmission']; ?></td>
+            </tr>
 
-                <div>
-                    <textarea id="additional_notes"
-                        class="bg-white text-black px-3 md:px-6 py-3 rounded-2xl focus:outline-none block w-80 md:w-4/5 mx-auto"
-                        name="additional_notes" rows="5" placeholder="Additional Notes"></textarea>
-                    <span class="text-sm font-medium w-80 md:w-4/5 mx-auto block">
-                        <?php if (!empty($error["msgErr"])) {
-                            echo $error["msgErr"];
-                        } ?>
-                    </span>
-                </div>
+            <tr class="border-b">
+                <th class="py-2 font-medium">Trim</th>
+                <td class="font-light"><?php echo $vehicleResArr['TRIM']; ?></td>
+            </tr>
 
-                <div>
-                    <button id="confirm_booking"
-                        class="block bg-[#513E04] text-white rounded-2xl px-3 md:px-6 py-3 w-80 md:w-4/5 mx-auto block my-5 hover:cursor-pointer hover:bg-[#7B5D01]">
-                        <i class="fa-solid fa-calendar-check"></i>
-                        Confirm Booking
-                    </button>
-                </div>
+            <tr class="border-b">
+                <th class="py-2 font-medium">Horsepower</th>
+                <td class="font-light"><?php echo $vehicleResArr['horsepower'] . " hp"; ?></td>
+            </tr>
 
-                <div id="set_message">
-                </div>
-            </form>
+            <tr class="border-b">
+                <th class="py-2 font-medium">Doors</th>
+                <td class="font-light"><?php echo $vehicleResArr['doors']; ?></td>
+            </tr>
+
+            <tr class="border-b">
+                <th class="py-2 font-medium">Fuel Type</th>
+                <td class="font-light"><?php echo $vehicleResArr['fuel_type']; ?></td>
+            </tr>
+
+            <tr class="border-b">
+                <th class="py-2 font-medium">No of Cylinders</th>
+                <td class="font-light"><?php echo $vehicleResArr['no_of_cylinders']; ?></td>
+            </tr>
+
+            <tr class="border-b">
+                <th class="py-2 font-medium">Interior Color</th>
+                <td class="font-light"><?php echo $vehicleResArr['interior_color']; ?></td>
+            </tr>
+
+            <tr class="border-b">
+                <th class="py-2 font-medium">Exterior Color</th>
+                <td class="font-light"><?php echo $vehicleResArr['exterior_color']; ?></td>
+            </tr>
+
+            <tr class="border-b">
+                <th class="py-2 font-medium">Per Day Cost</th>
+                <td class="font-light"><?php echo floor($vehicleResArr['per_day_cost']) . " AED / day"; ?></td>
+            </tr>
+
+            <tr class="border-b">
+                <th class="py-2 font-medium">Drive Type</th>
+                <td class="font-light"><?php echo $vehicleResArr['drive_type']; ?></td>
+            </tr>
+
+            <tr class="border-b">
+                <th class="py-2 font-medium">Seating Capacity</th>
+                <td class="font-light"><?php echo $vehicleResArr['seating_capacity']; ?></td>
+            </tr>
+
+            <tr class="border-b">
+                <th class="py-2 font-medium">Registration Number</th>
+                <td class="font-light"><?php echo $vehicleResArr['registration_number']; ?></td>
+            </tr>
+        </table>
+    </div>
+
+    <!-- Show vehicle booking form only when vehicle is available -->
+    <?php
+    $isBookRes = mysqli_query($isConnect, "SELECT * FROM vehicle_booking WHERE return_date < NOW() AND vehicle_id = " . $vehicleResArr['id']);
+    $isBookResArr = mysqli_fetch_assoc($isBookRes);
+    $bookedCount = mysqli_num_rows($isBookRes);
+
+    if ($bookedCount == 0) { ?>
+        <div class="w-full h-200 md:h-fit bg-cover flex flex-col justify-center items-center text-white mt-30 py-10 bg-no-repeat bg-scroll"
+            style="background-image:url('../Assets/contact_form_bg.png')">
+            <h4 class="text-2xl md:text-[45px] font-semibold mb-2 text-center my-20">Book This Car Now</h4>
+            <div class="w-80 md:w-4/5 mx-auto md:mb-5">
+                <form name="bookVehicle" id="bookVehicle" method="post" class="space-y-5">
+                    <input type="hidden" name="vehicle_id" id="vehicle_id" value="<?php echo $vehicleID; ?>">
+                    <div>
+                        <input type="text" id="pickup_date"
+                            class="bg-white text-black px-3 md:px-6 py-3 rounded-2xl focus:outline-none block w-80 md:w-4/5 mx-auto"
+                            name="pickup_date" placeholder="Select Pickup Date" onfocus="(this.type)='date'">
+                        <span class="text-sm font-medium w-80 md:w-4/5 mx-auto block">
+                            <?php if (!empty($error["nameErr"])) {
+                                echo $error["nameErr"];
+                            } ?>
+                        </span>
+                    </div>
+
+                    <div>
+                        <input type="text" id="return_date"
+                            class="bg-white text-black px-3 md:px-6 py-3 rounded-2xl focus:outline-none block w-80 md:w-4/5 mx-auto"
+                            name="return_date" placeholder="Select Return Date" onfocus="(this.type)='date'">
+                        <span class="text-sm font-medium w-80 md:w-4/5 mx-auto block">
+                            <?php if (!empty($error["emailErr"])) {
+                                echo $error["emailErr"];
+                            } ?>
+                        </span>
+                    </div>
+
+                    <div>
+                        <select
+                            class="bg-white text-black px-3 md:px-6 py-3 rounded-2xl focus:outline-none block w-80 md:w-4/5 mx-auto appearance-none"
+                            name="pickup_location" id="pickup_location">
+                            <option value="">Select Pickup Location</option>
+                            <?php
+                            foreach ($availableLoc as $val) { ?>
+                                <option value="<?php echo $val; ?>"><?php echo $val; ?></option>
+                            <?php }
+                            ?>
+                        </select>
+                        <span class="text-sm font-medium w-80 md:w-4/5 mx-auto block">
+                            <?php if (!empty($error["phoneErr"])) {
+                                echo $error["phoneErr"];
+                            } ?>
+                        </span>
+                    </div>
+
+                    <div>
+                        <select
+                            class="bg-white text-black px-3 md:px-6 py-3 rounded-2xl focus:outline-none block w-80 md:w-4/5 mx-auto appearance-none"
+                            name="is_driver_needed" id="is_driver_needed">
+                            <option value="">Need a Driver?</option>
+                            <option value="Yes">Yes</option>
+                            <option value="No">No</option>
+                        </select>
+                        <span class="text-sm font-medium w-80 md:w-4/5 mx-auto block">
+                            <?php if (!empty($error["phoneErr"])) {
+                                echo $error["phoneErr"];
+                            } ?>
+                        </span>
+                    </div>
+
+                    <div>
+                        <textarea id="additional_notes"
+                            class="bg-white text-black px-3 md:px-6 py-3 rounded-2xl focus:outline-none block w-80 md:w-4/5 mx-auto"
+                            name="additional_notes" rows="5" placeholder="Additional Notes"></textarea>
+                        <span class="text-sm font-medium w-80 md:w-4/5 mx-auto block">
+                            <?php if (!empty($error["msgErr"])) {
+                                echo $error["msgErr"];
+                            } ?>
+                        </span>
+                    </div>
+
+                    <div>
+                        <button id="confirm_booking"
+                            class="block bg-[#513E04] text-white rounded-2xl px-3 md:px-6 py-3 w-80 md:w-4/5 mx-auto block my-5 hover:cursor-pointer hover:bg-[#7B5D01]">
+                            <i class="fa-solid fa-calendar-check"></i>
+                            Confirm Booking
+                        </button>
+                    </div>
+
+                    <div id="set_message">
+                    </div>
+                </form>
+            </div>
+        </div>
+    <?php } ?>
+<?php } else { ?>
+    <div class="w-full min-h-[60vh] flex flex-col justify-center items-center text-center p-6 bg-gray-50">
+        <div class="text-[#7B5D01] text-7xl md:text-9xl font-bold mb-4 opacity-20">404</div>
+        
+        <h2 class="text-2xl md:text-4xl font-semibold text-red-800 mb-2">Vehicle Not Found</h2>
+        
+        <p class="text-gray-600 max-w-md mx-auto mb-8 text-sm md:text-base">
+            The car profile you are looking for might have been removed, booked out, or does not exist in our premium fleet.
+        </p>
+        
+        <div class="flex flex-col sm:flex-row gap-4 justify-center">
+            <a href="car_listing.php" 
+               class="bg-[#7B5D01] hover:bg-[#3b3112] text-white px-3 py-2 text-xs rounded-md font-medium flex items-center gap-2 transition duration-200">
+                <i class="fa-solid fa-car"></i> View Available Fleet
+            </a>
+            <a href="contact_us.php" 
+               class="border border-gray-300 hover:bg-gray-100 text-gray-700 px-3 py-2 text-xs rounded-md font-medium flex items-center gap-2 transition duration-200">
+                <i class="fa-regular fa-paper-plane"></i> Contact Support
+            </a>
         </div>
     </div>
 <?php } ?>
+
+
+
 
 
 <?php

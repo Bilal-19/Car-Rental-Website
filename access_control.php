5799b0af4079ba3f16ef5c23493aecbc728e14c5
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2026-08-02 18:53:44 +0500

    prevent unauthorized access to admin dashboard

--- access_control.php
+++ access_control.php
@@ -1,8 +1,10 @@
 <?php
 session_start();
-if (isset($_SESSION['useremail'])) {
-    if ($_SESSION['useremail'] != 'bilalmuhammadyousuf543@gmail.com') {
-        header("Location: ../index.php");
-    }
+if (
+    !isset($_SESSION['useremail']) ||
+    $_SESSION['useremail'] != 'bilalmuhammadyousuf543@gmail.com'
+) {
+    header("Location: ../Admin/login.php");
+    exit;
 }
 ?>
